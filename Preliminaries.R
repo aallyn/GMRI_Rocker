2605ff24f4c34072ea3dff541141ebbcdd7bad36
aallyn <Andrew.Allyn@gmail.com>
2020-09-27 13:44:27 -0400

    VAST v3.5.0

--- Preliminaries.R
+++ Preliminaries.R
@@ -17,7 +17,7 @@ require(FishStatsUtils)
 install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
 require(INLA)
 
-install_github("james-thorson/VAST", ref = "3.5.5")
+install_github("james-thorson/VAST", ref = "3.5.0")
 require(VAST)
 
 install.packages("here")
