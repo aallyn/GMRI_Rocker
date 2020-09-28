0193447e080c8cdf98bd04431d21ca74b38ce070
aallyn <Andrew.Allyn@gmail.com>
2020-09-27 20:32:12 -0400

    New edits to try to get VAST working

--- Preliminaries.R
+++ Preliminaries.R
@@ -11,13 +11,13 @@ require(Matrix)
 install.packages("TMB")
 require(TMB)
 
-install_github("james-thorson/FishStatsUtils", INSTALL_opts="--no-staged-install")
+install_github("james-thorson/FishStatsUtils", ref = "2.7.0", INSTALL_opts="--no-staged-install")
 require(FishStatsUtils)
 
 install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
 require(INLA)
 
-install_github("james-thorson/VAST", ref = "3.5.0")
+install_github("james-thorson/VAST", ref = "3.5.5")
 require(VAST)
 
 install.packages("here")
