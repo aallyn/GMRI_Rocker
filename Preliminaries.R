d0502a50d36756dc2f1ba7c0310c844b3337d269
Andrew Allyn <aallyn@gmris-MBP.iHAR>
2023-02-27 21:00:36 -0500

    Small edit to R stuff again

--- Preliminaries.R
+++ Preliminaries.R
@@ -3,6 +3,7 @@
 #####
 
 # New as of 2023-02-27
-install.packages("TMB", type = "source")
+install.packages("Matrix")
+devtools::install_github("kaskr/adcomp/TMB", dependencies = TRUE, force = TRUE)
 library(TMB)
 remotes::install_github("james-thorson/VAST", ref = "ae94aaa81db4996e4355ded1e947d33c2140f0c8", dep = TRUE")
