16af1c63f78f766cabaa9b73a34b80c49d37b2ec
Andrew Allyn <aallyn@gmris-MacBook-Pro.local>
2023-03-01 17:07:59 -0500

    With INLA and VAST

--- Preliminaries.R
+++ Preliminaries.R
@@ -7,6 +7,6 @@ install.packages("Matrix")
 # devtools::install_github("kaskr/adcomp/TMB", dependencies = TRUE, force = TRUE)
 install.packages("TMB", type = "source")
 library(TMB)
-# install.packages("INLA", repos = c(INLA = "https://inla.r-inla-download.org/R/testing"), dep = TRUE)
-# remotes::install_github("james-thorson/VAST", ref = "ae94aaa81db4996e4355ded1e947d33c2140f0c8", dep = TRUE)
+install.packages("INLA", repos = c(INLA = "https://inla.r-inla-download.org/R/testing"), dep = TRUE)
+remotes::install_github("james-thorson/VAST", ref = "ae94aaa81db4996e4355ded1e947d33c2140f0c8", dep = TRUE)
 
